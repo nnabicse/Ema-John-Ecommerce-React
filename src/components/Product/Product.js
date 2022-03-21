7d69f131d9edf467950e85c1c8f1b2c3613df7cd
Nurun Nabi <nnabi.cse@gmail.com>
2022-03-22 00:52:04 +0600

    product images display complete

---
+++ src/components/Product/Product.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props.product)
+    const {img, name, seller, price, ratings} = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <p className='product-name'>{name}</p>
+
+        </div>
+    );
+};
+
+export default Product;
