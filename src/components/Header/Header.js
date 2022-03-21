7d50b7b8afc02eaf25b8928624c96930ae9b6f58
Nurun Nabi <nnabi.cse@gmail.com>
2022-03-22 00:03:50 +0600

    header logo and anchors added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
 
 const header = () => {
     return (
         <div>
-            <h2>This is a Header</h2>
+            <nav className="header">
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/shop">Shop</a>
+                    <a href="/orders">Orders</a>
+                    <a href="/inventory">Inventory</a>
+                    <a href="/about">About</a>
+                </div>
+            </nav>
         </div>
     );
 };
