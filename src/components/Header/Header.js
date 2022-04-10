c4077a9114def86a77d1ddaee07f6cfbe6179dc1
Nurun Nabi <nnabi.cse@gmail.com>
2022-04-10 17:43:55 +0600

    signOut implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
@@ -7,7 +8,9 @@ import './Header.css'
 
 const Header = () => {
     const [user] = useAuthState(auth)
-    console.log(user);
+    const handleSignOut = () =>{
+        signOut(auth)
+    }
     return (
         <div>
             <nav className="header">
@@ -18,7 +21,7 @@ const Header = () => {
                     <Link to="/inventory">Inventory</Link>
                     <Link to="/about">About</Link>
                     {
-                        user ? <button>Sign Out</button> : <Link to='/login'>Login</Link>
+                        user ? <button onClick={handleSignOut}>Sign Out</button> : <Link to='/login'>Login</Link>
                     }
                 </div>
             </nav>
